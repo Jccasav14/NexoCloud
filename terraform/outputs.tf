adf2d5d1e841a2f2f6256df549c60570e520b462
Jccasav <jeancasaxd60@gmail.com>
2026-07-12 10:46:32 -0500

    docs: add header to output variable definitions

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,4 @@
+# Terraform Output Variables for NexoCloud Deployment
 output "alb_dns_name" {
   description = "The public DNS name of the Application Load Balancer"
   value       = aws_lb.app_alb.dns_name
